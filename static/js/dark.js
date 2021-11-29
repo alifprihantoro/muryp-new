9176fef400f102bd202f7f5c12dd6b237d669d13
alifmuryp <alifmuryp@gmail.com>
2021-11-29 13:31:49 +0700

    fix dark btn

---
+++ static/js/dark.js
@@ -0,0 +1,9 @@
+  if (localStorage.getItem('theme') == 'dark')
+  setDarkMode()
+function setDarkMode() {
+  let isDark = document.body.classList.toggle("dark-mode")
+  if (isDark) {
+    localStorage.setItem('theme', 'dark')} else {
+    localStorage.removeItem('theme')}
+  document.getElementById('btn-dark').innerHTML
+}
