9d1857436a8887a5dab4bb7eaaad71db1b60b6c8
alifmuryp <alifmuryp@gmail.com>
2021-12-02 13:51:29 +0700

    fail

--- build/alias.sh
+++ build/alias.sh
@@ -1,17 +1,9 @@
 alias dmb1='
-# cp script
-source $dmb/params/script.sh
 # config
 source $dmb/params/config.sh
-# head
-rm $dmtp/main/head.html
-cp $dmtp/main/.online $dmntp/main/head.html
 # build
 dm
 hugo --minify
-# back offline
-rm $dmtp/main/head.html
-cp $dmtp/main/.offline $dmntp/main/head.html
 '
 
 alias dmtss1='
@@ -20,5 +12,5 @@ sass --watch --style=compressed scss:css
 '
 
 alias dmtsj1='
-ls $dmtsj/params/* | entr -r $dmnsj/js.sh
+ls $dmtsj/params/* | entr -r $dmtsj/params/init.sh
 '
--- build/params/script.sh
+++
@@ -1,2 +0,0 @@
-rm $dmns/css
-cp $dmntps/css $dmns/
