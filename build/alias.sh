7223a480cae820d5118f4ee2f5a5a9084539aded
alifmuryp <alifmuryp@gmail.com>
2021-12-02 14:28:28 +0700

    fix alias

--- build/alias.sh
+++ build/alias.sh
@@ -1,16 +1,16 @@
-alias dmb1='
 # config
-source $dmbs/init.sh
+alias dmbs1='source $dmbs/init.sh'
+alias dmtss1='dmts && sass --watch --style=compressed scss:css'
+alias dmtsj1='source $dmtsjp/init.sh'
+alias dmbs2='ls $dmtsjp/* | entr -r dmbs1'
+alias dmtsj2='ls $dmtsjp/* | entr -r dmtsj1'
+
+# deploy
+alias dmb1='
 # build
 dm
-hugo --minify
-'
-
-alias dmtss1='
+dmbs1
 dmtss1
-sass --watch --style=compressed scss:css
-'
-
-alias dmtsj1='
-ls $dmtsjp/* | entr -r $dmtsjp/init.sh
+dmtsj1
+hugo --minify
 '
