08afe1fb7842f2ee89b8a096061100a6cb3d742e
alifmuryp <alifmuryp@gmail.com>
2021-12-02 16:21:06 +0700

    fix theme ver.1.1.1

---
+++ themes/muryp.v.1.2.1/static/js/darkmode.js
@@ -0,0 +1,10 @@
+if(localStorage.getItem('theme') == 'dark')
+  setDarkMode()
+function setDarkMode() {
+  let isDark = document.body.classList.toggle("dark-mode")
+  if(isDark){
+  localStorage.setItem('theme', 'dark')}
+  else {
+  localStorage.removeItem('theme')}
+  document.getElementById('darkBtn').innerHTML 
+}
