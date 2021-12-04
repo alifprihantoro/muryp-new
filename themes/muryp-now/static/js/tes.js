ecd21085e1e6ec91d0657452fec0a3b92b6a9e0e
alifmuryp <alifmuryp@gmail.com>
2021-12-04 14:44:43 +0700

    fix json index

--- themes/muryp-now/static/js/tes.js
+++ themes/muryp-now/static/js/tes.js
@@ -1,18 +1,40 @@
-const filterItems = (arr, query) => {
-  return arr.filter(el => el.toLowerCase().indexOf(query.toLowerCase()) !== -1)
-}
-let url = "/index.json";
-fetch(url)
-  .then((res) => res.json())
-  .then((data) => {
-    const lengData = Object.keys(data).length;
-    for (let step = 0; step < lengData; step++) {
-      // Runs 5 times, with values of step 0 through 4.
-      // console.log(data[step].tags);
-      const myTags = data[step].tags;
-      // console.log(myTags);
-// console.log(filterItems(myTags, 'ja'))
-      const tes1 = filterItems(myTags, 'ja')
-      console.log(tes1)
-    }
-  });
+
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const filterItems = (arr, query) => {
+//   return arr.filter(el => el.toLowerCase().indexOf(query.toLowerCase()) !== -1)
+// }
+// let url = "/index.json";
+// fetch(url)
+//   .then((res) => res.json())
+//   .then((data) => {
+//     const lengData = Object.keys(data).length;
+//     for (let step = 0; step < lengData; step++) {
+//       // Runs 5 times, with values of step 0 through 4.
+//       // console.log(data[step].tags);
+//       const myTags = data[step].tags;
+//       // console.log(myTags);
+// // console.log(filterItems(myTags, 'ja'))
+//       const tes1 = filterItems(myTags, 'ja')
+//       console.log(tes1)
+//     }
+//   });
