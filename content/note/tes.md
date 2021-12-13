e84f10d376d6faac71d5de93a5ca2fdcb6c8cbd0
alifmuryp <alifmuryp@gmail.com>
2021-12-14 06:33:14 +0700

    render link

---
+++ content/note/tes.md
@@ -0,0 +1,23 @@
+---
+title: "Title"
+slug: slug
+date: 2021-12-14T05:11:31+07:00
+lastmod: 2021-12-14T05:11:31+07:00
+draft: false
+show_comments: false
+
+type: post
+
+tags:
+    - Tag
+
+image: ""
+thumbnail : ""
+
+description: ""
+
+---
+[link external](https://murypstudio.my.id/)
+[link internal](/blog)
+[link internal](/note)
+[link internal](https://muryp.my.id/blog)
--- themes/muryp-now/static/js/tes-class.js
+++ themes/muryp-now/static/js/tes-class.js
@@ -1,3 +1,15 @@
+const link = (e) => {
+  alert(e);
+  const nextTitle = "My new page";
+  const nextState = { additionalInformation: "Updated the URL with JS" };
+
+  // This will create a new entry in the browser's history, without reloading
+  window.history.pushState(nextState, nextTitle, e);
+
+  // This will replace the current entry in the browser's history, without reloading
+  window.history.replaceState(nextState, nextTitle, e);
+  return false;
+};
 // // unnamed
 // let Rectangle = class {
 //   constructor(height, width, name) {
