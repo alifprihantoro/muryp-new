66e6fcfbd96521b9eb58e465578a7287c0c010fe
alifprihantoro <alifmuryp@gmail.com>
2022-04-10 21:25:13 +0700

    reset and add layout

--- assets/js/.not-use/btn-post.js
+++
@@ -1,4 +0,0 @@
-const btn_post = `
-<button class="btn-close" onclick="backLink()">Back</button>
-<button class="btn-close" onclick="link('/')">close</button>
-`
--- assets/js/.not-use/tag-klick.js
+++
@@ -1,5 +0,0 @@
-const btn_list_tag = ()=>{
-  const url_list = document.getElementById("list-tag-search");
-  const tagName = $chekList(url_list);
-  console.log(tagName)
-}
