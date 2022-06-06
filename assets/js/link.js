8e2ee1973f81eba58f6d4383577030ebdf9e93e1
alifprihantoro <alifmuryp@gmail.com>
2022-06-06 09:47:05 +0700

    fixed window

--- assets/js/link.js
+++ assets/js/link.js
@@ -1,20 +1,19 @@
-// togle
-const togleWindow = (id)=>{
-  // console.log(id)
-  document.querySelector(`#${id}`).classList.toggle('hidden');
-}
 // get link
-const myId = ()=>{
-	// The URL changed...
-  const link = window.location.href
+const myId = () => {
+  // The URL changed...
+  let link = window.location.href
   // console.log(link)
-  const my_id = link.split("#")[1]
-  // console.log(my_id)
-  togleWindow(my_id)
+  let id = link.split('#')[1]
+  // console.log(id)
+  document.querySelector(`#${id}`).classList.toggle('hidden')
+}
+const closeWindow = () => {
+  myId()
+  window.open('/#', '_self')
 }
 // samakan kayak klick url
 window.addEventListener('popstate', function () {
-myId()
-});
+  myId()
+})
 
 myId()
