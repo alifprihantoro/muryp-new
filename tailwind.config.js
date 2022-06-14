ec07683c208846b5cf8e7f01048213d6e9481bd3
alifprihantoro <alifmuryp@gmail.com>
2022-06-14 16:38:58 +0700

    fail

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: ['./layouts/partials/home/**/*.{html,js}','./layouts/index.html'],
+  theme: {
+    extend: {
+      colors: {
+        main: {
+          1: '#035a0e',
+          2: '#038811',
+          3: '#35a041',
+          3: '#b3f166',
+        }
+      },
+    },
+  plugins: [],
+  },
+}
